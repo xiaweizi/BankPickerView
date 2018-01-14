82372bb8a343b27ef9ac8e9644782476ebebfb84
xiaweizi <1012126908@qq.com>
2018-01-14 16:54:41 +0800

    初次提交

---
+++ app/src/main/java/com/xiaweizi/bankpickerview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.xiaweizi.bankpickerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BankPickerView mPickerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPickerView = findViewById(R.id.bank_picker_view);
+
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data.add("数据" + i);
+        }
+        mPickerView.setOffset(3);
+        mPickerView.setItems(data);
+        mPickerView.setOnWheelViewListener(new BankPickerView.OnWheelViewListener() {
+            @Override
+            public void onSelected(int selectedIndex, String item) {
+                Log.d("BankPickerView:", "selectedIndex: " + selectedIndex + ", item: " + item);
+            }
+        });
+    }
+
+}
